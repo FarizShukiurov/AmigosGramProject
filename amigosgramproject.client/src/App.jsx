1f940895514941ba83560bf15acf8b70be50c301
Exem3l <119063714+Exem3l@users.noreply.github.com>
2024-09-26 16:39:58 +0400

    Contacts, layouts, redesign

--- amigosgramproject.client/src/App.jsx
+++ amigosgramproject.client/src/App.jsx
@@ -1,16 +1,21 @@
+/* eslint-disable no-unused-vars */
 import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
 import Login from './components/Login';
-import Register from './components/Register';
-import Home from './components/Home';
+import Layout from './components/Layout';
+import Contacts from './components/Contacts';
 
 function App() {
     return (
         <Router>
             <Routes>
                 <Route path="/login" element={<Login />} />
-                <Route path="/" element={<Home />} />
-                <Route path="/register" element={<Register />} />
+                <Route path="/" element={<Layout />}>
+                    <Route path="contacts" element={<Contacts />} />
+                    
+
+                </Route>
+                <Route path="*" element={"Not found" }></Route>
             </Routes>
         </Router>
     );

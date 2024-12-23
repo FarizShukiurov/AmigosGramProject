cbd39707ed99afe5b9633127a2fa269e9f9c6cb6
FarizShukiurov <farashuk2004@gmail.com>
2024-12-23 21:29:59 +0400

    Group

---
+++ AmigosGramProject.Server/DTOs/ParticipantDto.cs
@@ -0,0 +1,9 @@
+﻿namespace AmigosGramProject.Server.DTOs
+{
+    public class ParticipantDto
+    {
+        public string UserId { get; set; } 
+        public string EncryptedGroupKey { get; set; } 
+    }
+
+}
---
+++ AmigosGramProject.Server/Models/GroupMember.cs
@@ -0,0 +1,11 @@
+﻿namespace AmigosGramProject.Server.Models
+{
+    public class GroupMember
+    {
+        public Guid Id { get; set; }
+        public Guid GroupId {  get; set; }
+        public string UserId { get; set; }
+        public string EncryptedGroupKey { get; set; }
+        public DateTime JoinedAt { get; set; }
+    }
+}
